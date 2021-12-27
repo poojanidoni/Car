e4cca78858e39390a4b0e5d963d17bf1f60fa31b
poojanidoni <poojanidonixworksz@gmail.com>
2021-12-27 12:23:38 +0530

    String argument

---
+++ Car.java
@@ -0,0 +1,10 @@
+class Car{
+	public static void main(String []riya){
+		System.out.println("main() started");
+	for(int index=0; index<riya.length; index++){
+		System.out.println(riya[index]);
+	}
+		System.out.println("End of the main()");
+		
+	}
+}
